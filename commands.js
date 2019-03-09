08bceb2cf5fd9a3c19db8cda90e49123d348639b
jrshutske <jackshutske@gmail.com>
2019-03-09 11:50:08 -0600

    revamp roll to user switch

--- commands.js
+++ commands.js
@@ -3,19 +3,25 @@ const say = require('./methods/say.js')
 const music = require('./methods/music.js')
 const slots = require('./methods/slots.js')
 const greeting = require('./methods/greeting.js')
+const roll = require('./methods/roll.js')
 
 const command = (msgContent) => {
   switch(msgContent.COMMAND) {
-    case "char"   : return char.main(msgContent);
-    case "say"    : return say.main(msgContent);
-    case "play"   : return music.play(msgContent);
-    case "join"   : return music.join(msgContent);
-    case "add"    : return music.add(msgContent);
-    case "q"      : return music.q(msgContent);
-    case "search" : return music.search(msgContent);
-    case "slots"  : return slots.play(msgContent);
-    case "hi"     : return greeting.greet(msgContent);
-    default:return null;
+    case "char"   : return char.main(msgContent)
+    case "say"    : return say.main(msgContent)
+    case "slots"  : return slots.main(msgContent)
+    case "roll"   : return roll.main(msgContent)
+    case "play"   : return music.play(msgContent)
+    case "join"   : return music.join(msgContent)
+    case "add"    : return music.add(msgContent)
+    case "q"      : return music.q(msgContent)
+    case "search" : return music.search(msgContent)
+    case "yo"     :
+    case "hi"     :
+    case "sup"    :
+    case "hey"    :
+    case "hello"  : return greeting.greet(msgContent)
+    default:return null
   }
 }
 module.exports.command = command
--- methods/roll.js
+++ methods/roll.js
@@ -1,28 +1,36 @@
-module.exports = {
-  'customRoll': (msg) => {
-    let roll;
-		let rollmax = msg.content.split(' ')[1];
-				if (!rollmax || rollmax < 1) {
-						msg.reply("Please specify a number of sides!");
-						return;
-				}
-				else if (isNaN(rollmax)) {
-						msg.reply("That's not a number.");
-						return;
-				}
-    roll = Math.floor(Math.random()*(rollmax-1+1)+1);
-    msg.reply({
-      embed: {
-        color: 3447003,  description: "You rolled a " + roll +  "."
-      }
-    });
-  },
-  'basicRoll': (msg) => {
-    let roll;
+const main = (msgContent) => {
+  msgContent.ACTION == undefined ?
+  basic(msgContent) :
+  custom(msgContent);
+}
+
+const custom = (msgContent) => {
+  let msg = msgContent.msg;
+  let roll;
+	let rollmax = msgContent.ACTION;
+			if (!rollmax || rollmax < 1) {
+					msg.reply("Please specify a number of sides!");
+					return;
+			}
+			else if (isNaN(rollmax)) {
+					msg.reply("That's not a number.");
+					return;
+			}
+  roll = Math.floor(Math.random()*(rollmax-1+1)+1);
+  msg.reply({
+    embed: {
+      color: 3447003,  description: "You rolled a " + roll +  "."
+    }
+  });
+}
+
+const basic = (msgContent) => {
+  let msg = msgContent.msg;
+  let roll;
     roll = Math.floor(Math.random()*(6-1+1)+1);
     msg.reply({
       embed: {color: 3447003,  description: "You rolled a " + roll +  "."}
     });
   }
-  
-}
+
+module.exports.main = main
