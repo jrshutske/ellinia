e3a200a3e620ad4711b2248d122096c712ad6ee3
jrshutske <jackshutske@gmail.com>
2019-05-29 01:07:35 -0500

    user ES6, fetch and proper promises

--- methods/character.js
+++ methods/character.js
@@ -1,97 +1,102 @@
-const { Client, RichEmbed } = require('discord.js')
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  const { Client, RichEmbed } = require('discord.js')
+  const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  const fetch = require("node-fetch");
 
-const main = (msgContent) => {
-  msg = msgContent.msg;
-  client = msgContent.client;
-  realm = msgContent.ARGS[1];
-  character = msgContent.ARGS[2];
-  getToken();
-}
-
-// fetch('http://example.com/movies.json')
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(myJson) {
-//     console.log(JSON.stringify(myJson));
-//   });
-const getCharacter = () => {
-  let xhr2 = new XMLHttpRequest();
-  let url = `https://us.api.blizzard.com/wow/character/${realm}/${character}?access_token=${token}`;
-  xhr2.open("get", url);
-  xhr2.onreadystatechange = () => {
-    if(xhr2.readyState == 4) {
-      let jsonr = JSON.parse(xhr2.responseText);
-      if (jsonr.reason == "Character not found.") {
-        msg.channel.send("Character not found!");
-      }
-      if (jsonr.reason == "Realm not found.") {
-        msg.channel.send("Realm not found!");
-      }
-      if (jsonr.reason == null) {
-        realmName = jsonr.realm
-        name = jsonr.name
-        level = jsonr.level
-        achievementPoints = jsonr.achievementPoints
-        totalHonorableKills = jsonr.totalHonorableKills
-        thumbnail = jsonr.thumbnail;
-        className = getClassName(jsonr.class);
-
-      }
-    }
+  const main = (msgContent) => {
+    let msg = msgContent.msg;
+    let client = msgContent.client;
+    let realm = msgContent.ARGS[1];
+    let character = msgContent.ARGS[2];
+    getCharacterData(msg, client, realm, character);
   }
-  xhr2.send(null);
-}
 
-const getToken = () => {
-  let xhr = new XMLHttpRequest();
-  let url = `https://us.battle.net/oauth/token?client_id=f0315fe57d76491695b77140f61ffda3&client_secret=MFVeGxsAhQyTIxWt0SJMhxaE7c87ioSv&grant_type=client_credentials`;
-  xhr.open("get", url);
-  xhr.onreadystatechange = () => {
-    if(xhr.readyState == 4) {
-      let jsonr = JSON.parse(xhr.responseText);
-      token = jsonr.access_token
-      getCharacter(token)
-    }
-  }
-  xhr.send(null);
-}
-
-const getClassName = (classNum) => {
-  let xhr = new XMLHttpRequest();
-  let url = `https://us.api.blizzard.com/wow/data/character/classes?locale=en_US&access_token=${token}`;
-  xhr.open("get", url);
-  xhr.onreadystatechange = () => {
-    if(xhr.readyState == 4) {
-      let jsonr = JSON.parse(xhr.responseText);
-      jsonr.classes.forEach(function(element) {
-        if (classNum == element.id) {
-          msg.channel.send(buildCharEmbed(element.name));
+  const getCharacter = (realm, character, token) => {
+    return new Promise((resolve, reject) => {
+    fetch(`https://us.api.blizzard.com/wow/character/${realm}/${character}?access_token=${token}`)
+      .then((response) =>  response.json())
+      .then((jsonr) => {
+        let characterObj = {};
+        if (jsonr.reason == null) {
+          characterObj.realm = jsonr.realm
+          characterObj.name = jsonr.name
+          characterObj.level = jsonr.level
+          characterObj.achievementPoints = jsonr.achievementPoints
+          characterObj.totalHonorableKills = jsonr.totalHonorableKills
+          characterObj.thumbnail = jsonr.thumbnail;
+          characterObj.class = jsonr.class;
+          resolve(characterObj);
         }
-      });
-    }
+        if (jsonr.reason) {
+          reject(Error(jsonr.reason));
+          msg.channel.send("Character not found!");
+        }
+      })
+    })
   }
-  xhr.send(null);
-}
 
-const buildCharEmbed = (className) => {
-  let classIcon = className.trim().toLowerCase().replace(/\s/g, '');
-  let tnstr = thumbnail.replace("avatar", "main");
-  const embed = new RichEmbed()
-    .setTitle(name)
-    .setColor(000000)
-    .setDescription(realmName, true)
-    .addField("Level:", level, true)
-    .addField("Class:", className, true)
-    .addField("Achievement Points:", achievementPoints, true)
-    .addField("Honorable Kills:", totalHonorableKills, true)
-    .setThumbnail(`https://wow.zamimg.com/images/wow/icons/large/classicon_${classIcon}.jpg`)
-    .setImage(`http://render-us.worldofwarcraft.com/character/${tnstr}`)
-    .setTimestamp()
-    .setURL(`https://worldofwarcraft.com/en-us/character/us/${realm}/${character}`)
-    .setFooter("Character Data - Powered By Peasant", client.user.avatarURL)
-  return {embed};
-}
+  const getToken = () => {
+    return new Promise((resolve, reject) => {
+    fetch(`https://us.battle.net/oauth/token?client_id=f0315fe57d76491695b77140f61ffda3&client_secret=MFVeGxsAhQyTIxWt0SJMhxaE7c87ioSv&grant_type=client_credentials`)
+      .then((response) =>  response.json())
+      .then((myJson) => {
+        let token = myJson.access_token
+        if ( token ) {
+          resolve(token);
+        } else {
+          reject(Error("Could not get an access token."));
+        }
+      })
+    })
+  }
 
-module.exports.main = main;
+  const  getClassName  = (token, classNum) => {
+    return new Promise((resolve, reject) => {
+      fetch(`https://us.api.blizzard.com/wow/data/character/classes?locale=en_US&access_token=${token}`)
+        .then((response) =>  response.json())
+        .then((myJson) => {
+          let className;
+          myJson.classes.forEach((element) =>{
+            if (classNum == element.id) {
+              className = element.name;
+            }
+          })
+          className ? resolve(className) : reject(Error("Could not get an access token."))
+        })
+      })
+  }
+
+  const  buildEmbed  = (characterObj, client) => {
+    return new Promise((resolve)  => {
+      let classIcon = characterObj.class.trim().toLowerCase().replace(/\s/g, '');
+      let tnstr = characterObj.thumbnail.replace("avatar", "main");
+      const embed = new RichEmbed()
+        .setTitle(characterObj.name)
+        .setColor(000000)
+        .setDescription(characterObj.realm, true)
+        .addField("Level:", characterObj.level, true)
+        .addField("Class:", characterObj.class, true)
+        .addField("Achievement Points:", characterObj.achievementPoints, true)
+        .addField("Honorable Kills:", characterObj.totalHonorableKills, true)
+        .setThumbnail(`https://wow.zamimg.com/images/wow/icons/large/classicon_${classIcon}.jpg`)
+        .setImage(`http://render-us.worldofwarcraft.com/character/${tnstr}`)
+        .setTimestamp()
+        .setURL(`https://worldofwarcraft.com/en-us/character/us/${characterObj.realm}/${characterObj.name}`)
+        .setFooter("Character Data - Powered By Peasant", client.user.avatarURL)
+      resolve({embed});
+    })
+  }
+
+  const getCharacterData = (msg, client, realm, character) => {
+    getToken().then((token) => {
+      getCharacter(realm, character, token).then((characterObj) => {
+        getClassName(token, characterObj.class).then((className) => {
+          characterObj.class = className;
+          buildEmbed(characterObj, client).then((embed)=>{
+            msg.channel.send(embed)
+          })
+        }).catch((error)=>console.log(error))
+      }).catch((error)=>console.log(error))
+    }).catch((error)=>console.log(error))
+  }
+
+  module.exports.main = main;
